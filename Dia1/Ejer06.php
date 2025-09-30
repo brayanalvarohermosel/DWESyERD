e8edb0ec63ebcd416efcc99779aa1f6c1fa5bb66
Brayan <balvarohermosel@iessonferrer.net>
2025-09-30 19:52:36 +0200

    Ejercicio6
    30/09/25

---
+++ Dia1/Ejer06.php
@@ -0,0 +1,6 @@
+<?php
+    $color = array('Blanco', 'Verde', 'Rojo');
+
+    echo($color[0] . ', ' .$color[1] . ', ' .$color[2] . ', ' . "\n· " .$color[1] . "\n· "  .$color[2] . "\n· " .$color[0]);
+
+?>
