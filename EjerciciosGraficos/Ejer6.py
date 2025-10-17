8a9de9c2c4149589c35ce4025d7e7e3fc0a03474
Brayan <balvarohermosel@iessonferrer.net>
2025-10-17 19:30:26 +0200

    Level2

---
+++ EjerciciosGraficos/Ejer6.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+# ● Genera una llista amb notes d’alumnes (per exemple 30 valors entre 0 i 10).
+# ● Representa un histograma amb 5 intervals (0–2, 2–4, 4–6, 6–8, 8–10).
+
